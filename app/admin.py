e1ce42cf7423777c3b8870042d9563721934dba1
Filipe Lucas <filipe.ines@cartrack.com>
2025-02-08 17:36:05 +0000

    remove pycache

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from app import models
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("handle",)
+
+admin.site.register(models.User, UserAdmin)
